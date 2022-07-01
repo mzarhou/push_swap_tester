e11168a1dc69bf69345a508f1b5461465c506074
Yahya El mrabti <yel-mrab@e3r3p14.1337.ma>
2022-07-01 01:46:00 +0100

    feat:highlighting status and adding some ok and ko emojis

--- tester.rb
+++ tester.rb
@@ -4,6 +4,7 @@ n = ARGV[0].to_i
 nb_tests = ARGV[1].to_i
 push_swap_path = "..".chomp('/')
 sizes_hash = {}
+check_arr = []
 
 #handling signals
 ["QUIT", "INT"].each { |signal|
@@ -27,8 +28,9 @@ nb_tests.times do
 	instructions_size = instructions.split("\n").size
 	# mapping number of instruction to associated random numbers
 	sizes_hash[instructions_size] = random_numbers
-	checker_status = `./#{push_swap_path}/push_swap #{random_numbers} | ./checker_Mac #{random_numbers}`
-	puts checker_status.chomp + " => " + instructions_size.to_s
+	checker_status = (`./#{push_swap_path}/push_swap #{random_numbers} | ./checker_Mac #{random_numbers}`).chomp
+	check_arr << checker_status if checker_status == "OK"
+	puts ((checker_status == "OK") ? "✅ " : "🟥 ") + checker_status + " => " + instructions_size.to_s
 end
 
 # list of instructions sizes of each test
@@ -40,7 +42,8 @@ puts "\n\nBenchmarks:"
 puts "min => " + sizes_list.min.to_s
 puts "max => " + max.to_s
 puts "avg => " + avg.to_s
-
+puts
+puts "You'v got " + check_arr.size.to_s + " ✅ and " + (nb_tests - check_arr.size).to_s + " 🟥 out of " + nb_tests.to_s + " tests\n"
 puts "\n\nFind worst case numbers in log file\n\n"
 
 File.write("log", sizes_hash[max])
